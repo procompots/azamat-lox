bf4ac3cc6264afc3c59b09759c6649df3cfba238
Abdullohabdumalikov <a3766668@gmail.com>
2025-03-05 22:48:37 +0500

    section2 va google fonts qoshildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-import './App.css'
+import "./App.css";
+import Input from "./componenets/AbdullohInput/input";
 
 function App() {
-
   return (
     <>
-      <header>
-
-      </header>
+      <header></header>
       <main>
-
+        <section className="sect2">
+          <Input/>
+        </section>
       </main>
-      <footer>
-
-      </footer>
+      <footer></footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/componenets/AbdullohInput/input.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./input.css";
+import sariqBola from  '../AbdullohInput/sariq bola.png'
+
+function Input() {
+  return (
+    <>
+      <div className="inputs">
+        <div className="inp-left">
+          <h2>СВЯЖИТЕСЬ С НАМИ ПО ЛЮБЫМ ВОПРОСАМ</h2>
+          <div className="inps">
+            <div className="inp1">
+              <input type="text" placeholder="Имя" />
+              <input type="email" placeholder="E-mail" />
+            </div>
+            <div className="inp2">
+              <input type="number" placeholder="Телефон" />
+              <input type="text" placeholder="Компания" />
+            </div>
+            <div className="inp3">
+              <textarea cols="30" rows="10" placeholder="Сообщение"></textarea>
+            </div>
+            <div className="inp4">
+              <button>Задать вопрос</button>
+            </div>
+          </div>
+        </div>
+        <div className="inp-right">
+          <img src={sariqBola} alt="" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Input;
