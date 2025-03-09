8d1c1c07d5d4dd1634ef1dd683764163b041060d
xasanov274 <xasanovabdulaziz51@gmail.com>
2025-03-09 13:53:58 +0500

    headerni togriladim

--- src/componenets/abdulazizHeader/header.jsx
+++ src/componenets/abdulazizHeader/header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './header.css'
+import "./header.css";
 import { MdOutlineSegment } from "react-icons/md";
 import { IoCartOutline } from "react-icons/io5";
 import { FaRegHeart } from "react-icons/fa";
@@ -8,27 +8,27 @@ function Header() {
   return (
     <div className="Wb">
       <div className="header">
-      <img src={image} alt="" />
-      <div className="pla">
-        <p className="p1">Trade In</p>
-        <p className="p1">Велосипеды</p>
-        <p className="p1">Запчасти</p>
-        <p className="p1">Экипировка</p>
-      </div>
-      <div className="icons">
-        <FaRegHeart />
-        <IoCartOutline />
-        <MdOutlineSegment />
-      </div>
+        <img src={image} alt="" />
+        <div className="pla">
+          <p className="p1">Trade In</p>
+          <p className="p1">Велосипеды</p>
+          <p className="p1">Запчасти</p>
+          <p className="p1">Экипировка</p>
+        </div>
+        <div className="icons">
+          <FaRegHeart style={{ fontSize: "20px" }} />
+          <IoCartOutline style={{ fontSize: "20px" }} />
+          <MdOutlineSegment style={{ fontSize: "30px" }} />
+        </div>
       </div>
       <div className="header2">
-      <h1 className="h11">Электро велосипеды</h1>
-      <p className="pca">
-        Cento10 Hybrid — это гоночный велосипед с помогающим <br />
-        педалированию электроприводом, который устанавливает новый,<br />
-        очень высокий стандарт для данной категории
-      </p>
-      <button className="buton">Подробнее</button>
+        <h1 className="h11">Электро велосипеды</h1>
+        <p className="pca">
+          Cento10 Hybrid — это гоночный велосипед с помогающим педалированию
+          электроприводом, который устанавливает новый, очень высокий стандарт
+          для данной категории
+        </p>
+        <button className="buton">Подробнее</button>
       </div>
     </div>
   );
