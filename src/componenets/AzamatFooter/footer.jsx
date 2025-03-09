a767b3d7cf9d0c430116ac47f23c528b0dd9aa4f
AzamatMurodjonov <murodjonovazamat07@gmail.com>
2025-03-09 12:00:51 +0500

    image done

---
+++ src/componenets/AzamatFooter/footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./footer.css";
+
+function Footer() {
+    return (
+        <>
+        <div className="header">
+            <div className="img">
+            </div>
+
+            <footer>
+
+            </footer>
+        </div>
+        
+        
+        </>
+    )
+}
+
+export default Footer
